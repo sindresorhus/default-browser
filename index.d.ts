27fa680bd901453e170afd73358cb723518b84ce
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-28 21:54:56 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,24 @@
-/**
-Gets the default browser on the platform.
-@returns a promise containing the human-readable name and bundle ID of the discovered browser.
- */
-declare function defaultBrowser(): Promise<defaultBrowser.Browser>;
-
 declare namespace defaultBrowser {
-	export interface Browser {
+	interface Browser {
 		/**
 		Human-readadable name of the browser.
-		 */
+		*/
 		name: string;
 
 		/**
 		Unique ID for the browser on the current platform:
-		- On OSX, this is the ID in LaunchServices.
-		- On Linux, it's the desktop file ID (from xdg-mime)
-		 */
+		- On macOS, it's the ID in LaunchServices.
+		- On Linux, it's the desktop file ID (from `xdg-mime`)
+		*/
 		id: string;
 	}
 }
+
+/**
+Get the default browser for the current platform.
+
+@returns A promise for the browser.
+*/
+declare function defaultBrowser(): Promise<defaultBrowser.Browser>;
 
 export = defaultBrowser;
